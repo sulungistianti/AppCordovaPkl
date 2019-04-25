0572b7eb9757d748b760b33f1587fa15633b11ea
sulungistianti <sulungistianti16@gmail.com>
2019-04-25 12:36:58 -0600

    sulung

---
+++ AppCordovaPkl/www/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("localhost","id8112587_admin1","sulungistianti","id8112587_admin1") or die ("could not connect database");
+?>
